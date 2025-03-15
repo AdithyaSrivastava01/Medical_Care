9191f8f7a8764c0ca4f3c3c52f3ac89054baec3a
Adithya Srivastava <adithyasrivastava01@gmail.com>
2025-03-15 03:04:27 -0700

    File Struct created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medi_care",
+    version = '0.0.0',
+    author = "Adithya Srivastava",
+    author_email="adithyasrivastava01@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
